beed69d74349c72d98fbb95e0d669ca79092f160
Kevin Loughead <kvnloughead@gmail.com>
2021-01-15 21:50:20 -0500

    Updates

---
+++ precache-manifest.f776f38329d27288d83caf2e49343673.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a436b42f322800cb104",
+    "url": "/static/css/main.e4e95a83.chunk.css"
+  },
+  {
+    "revision": "3a436b42f322800cb104",
+    "url": "/static/js/main.3a436b42.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5e30a12495bc0e7e0cbd77f3e5e1ea50",
+    "url": "/index.html"
+  }
+];
